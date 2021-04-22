cbcd151927cd0ca9ffa9bac5b0388861104c4014
Alexander <alexanderteodorocastro95@hotmail.com>
2021-04-22 19:47:24 -0300

    exercicio 3 finalizado

---
+++ exercises/exerc3.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 20;
+let c = 15;
+
+if (a > b && a > c) {
+    console.log('o maior número é: ' + a );
+}
+else if (b > c && b > a) {
+    console.log('o numero maior é: ' + b );
+}
+else {
+    console.log('o numero maior é: ' + c );
+
+};
